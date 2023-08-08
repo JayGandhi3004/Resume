6668a7dde6f1d5acfd584a3a37fdc1122b52699c
jay gandhi <jaygandhi3004@gmail.com>
2023-08-08 13:56:06 +0530

    resume

--- src/Component/About.js
+++ src/Component/About.js
@@ -18,7 +18,9 @@ const About = () => {
                 <h2>Jay Gandhi</h2>
               </div>
               <div className='about-data'>
-                <p>I am a passionate front-end developer with a keen eye for design and a love for creating intuitive user experiences.As a front-end developer, my passion lies in creating engaging and intuitive user experiences. With a strong foundation in HTML, CSS, and JavaScript, I craft responsive and mobile-friendly websites that captivate users across devices. My expertise extends to modern frameworks such as React allowing me to build dynamic and interactive web applications. My portfolio showcases a diverse range of projects, including e-commerce platforms, portfolio websites, and interactive web apps. Let's collaborate to transform your ideas into visually stunning and functionally seamless digital experiences.</p>
+                <p>I am a passionate front-end developer with a keen eye for design and a love for creating intuitive user experiences.</p>
+                <p>As a front-end developer, my passion lies in creating engaging and intuitive user experiences. With a strong foundation in HTML, CSS, and JavaScript, I craft responsive and mobile-friendly websites that captivate users across devices. My expertise extends to modern frameworks such as React allowing me to build dynamic and interactive web applications.</p>
+                <p>My portfolio showcases a diverse range of projects, including e-commerce platforms, portfolio websites, and interactive web apps. Let's collaborate to transform your ideas into visually stunning and functionally seamless digital experiences.</p>
                 <hr />
                 <div className='row'>
                   <div className='col-6'>
@@ -37,7 +39,7 @@ const About = () => {
                     <li style={{ color: "white", fontSize: "20px" }}><span className='d-block' style={{ color: "#f54942", fontSize: "20px" }}>2022</span> HSC Completed</li>
                     <li style={{ color: "white", fontSize: "20px" }}><span className='d-block' style={{ color: "#f54942", fontSize: "20px" }}>2023</span> BCA running</li>
                   </div>
-                  <hr/>
+                  <hr />
                 </div>
               </div>
             </div>
